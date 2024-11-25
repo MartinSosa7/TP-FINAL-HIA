cd527950828b774c1f7d99d2690c5e0203d06d14
Mauricio Miranda <129995731+MauricioMiranda6030@users.noreply.github.com>
2024-11-25 11:47:57 -0300

    otro back
    backend web

---
+++ backend/test/control.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+
+describe("test", () => {
+
+    const respuesta = true;
+
+    it("simple test", () => {
+        expect(respuesta).toBe(true)
+    })
+})
+
